8d71bb0cf7800ec258a6ece28fcca7ba01be1ece
codeajj <paavobro@gmail.com>
2025-03-03 13:13:10 +0200

    Changes!

--- Version 0.0.1/character_select.py
+++ Version 0.0.1/character_select.py
@@ -1,1 +1,27 @@
-selection = input("Choose your character: ")
+import mysql.connector
+import time
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="user",
+  password="test",
+  database="project_mars"
+)
+
+dbHaku = mydb.cursor()
+def tulos():
+    selection = dbHaku.fetchall()
+    return selection
+
+#Printaa kolme hahmoa tietokannan sisältä näyttääkseen
+dbHaku.execute("SELECT player FROM game;")
+#name = input("Choose your character 1-3: ")
+#dbHaku.execute(f"select {name}")
+
+print("Welcome to Mars rush!")
+time.sleep(1)
+print("You have three characters to select with varying difficulties!")
+time.sleep(3)
+print(tulos())
+
+#ALKEELLINEN TESTI
