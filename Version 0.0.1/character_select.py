c5b8a63ad52d5066ec8c4c1faff73e601ea5558c
codeajj <paavobro@gmail.com>
2025-03-03 16:55:52 +0200

    Branch luoto tästä ja päivitetty ...select.py

--- Version 0.0.1/character_select.py
+++ Version 0.0.1/character_select.py
@@ -15,14 +15,22 @@ def tulos():
 
 #Printaa kolme hahmoa tietokannan sisältä näyttääkseen
 dbHaku.execute("SELECT player FROM game;")
-#name = input("Choose your character 1-3: ")
-#dbHaku.execute(f"select {name}")
 
 print("Welcome to Mars Rush!")
 time.sleep(1)
 print("You have three characters to select with varying difficulties!")
 time.sleep(3)
 print(tulos())
+characters = tulos()
+
 print("Choose your character out of these three difficulties!")
+print("""
+      Normal
+      
+      Hard
+      
+      Extreme!
+""")
+
 
 #ALKEELLINEN TESTI
