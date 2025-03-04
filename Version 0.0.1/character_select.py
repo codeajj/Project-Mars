b79c9d1da974d537a829320940ee2d356e26b82a
codeajj <paavobro@gmail.com>
2025-03-04 13:40:25 +0200

    Branch luoto tästä ja päivitetty ...select.py

--- Version 0.0.1/character_select.py
+++ Version 0.0.1/character_select.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import time
+import re
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -8,11 +9,40 @@ mydb = mysql.connector.connect(
   database="project_mars"
 )
 
+#KAUNEUS FILTTERI ON!
 dbHaku = mydb.cursor()
 def tulos():
-    selection = dbHaku.fetchall()
+    ugly = dbHaku.fetchall()
+    selection = re.sub(r"[^a-zA-Z0-9 ]", "", str(ugly))
     return selection
 
+#HAHMOT:
+def Yrjö():
+    dbHaku.execute("SELECT player FROM game WHERE player = 'Yrjö';")
+    print(f"Character: ", tulos())
+    dbHaku.execute("SELECT wallet FROM game WHERE player = 'Yrjö';")
+    print(f"Wallet: ", tulos())
+    dbHaku.execute("SELECT airport.name as 'Airport' FROM airport, game WHERE location = ident and player = 'Yrjö';")
+    print(f"Location: ", tulos())
+    return
+def Hasan():
+    dbHaku.execute("SELECT player FROM game WHERE player = 'Hasan';")
+    print(f"Character: ", tulos())
+    dbHaku.execute("SELECT wallet FROM game WHERE player = 'Hasan';")
+    print(f"Wallet: ", tulos())
+    dbHaku.execute("SELECT airport.name as 'Airport' FROM airport, game WHERE location = ident and player = 'Hasan';")
+    print(f"Location: ", tulos())
+    return
+def SumTinWong():
+    dbHaku.execute("SELECT player FROM game WHERE player = 'Sum Tin Wong';")
+    print(f"Character: ", tulos())
+    dbHaku.execute("SELECT wallet FROM game WHERE player = 'Sum Tin Wong';")
+    print(f"Wallet: ", tulos())
+    dbHaku.execute("SELECT airport.name as 'Airport' FROM airport, game WHERE location = ident and player = 'Sum Tin Wong';")
+    print(f"Location: ", tulos())
+    return
+
+#INTRO?!?!? Tosi bad veri baad
 print("Welcome to Mars Rush!")
 time.sleep(1)
 print("You have three characters to select with varying difficulties!")
@@ -29,16 +59,13 @@ print("""
 diff = input("Choose your difficulty: ")
 
 if diff == "1" or "Normal" or "normal":
-    dbHaku.execute("SELECT player FROM game WHERE player = 'Yrjö';")
-    print(f"Character: ", tulos())
-    dbHaku.execute("SELECT wallet FROM game WHERE player = 'Yrjö';")
-    print(f"Wallet: ", tulos())
-    dbHaku.execute("SELECT airport.name as 'Airport' FROM airport, game WHERE location = ident and player = 'Yrjö';")
-    print(f"Location: ", tulos())
+    Yrjö()
 
 elif diff == "2" or "Hard" or "hard":
-    dbHaku.execute("SELECT player FROM game WHERE player = 'Hasan';")
-    print(tulos())
+    Hasan()
+
+elif diff == "3" or "Extreme" or "extreme":
+    SumTinWong()
 else:
-    dbHaku.execute("SELECT player FROM game WHERE player = 'Sum Tin Wong';")
-    print(tulos())
+    print("Error!")
+    diff = input("Choose your difficulty: ")
