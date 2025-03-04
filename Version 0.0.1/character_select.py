ef474c7ece3d29b5b89cc6ecf6d3a5bdc9823a34
codeajj <paavobro@gmail.com>
2025-03-04 14:38:02 +0200

    Branch luoto tästä ja päivitetty ...select.py

--- Version 0.0.1/character_select.py
+++ Version 0.0.1/character_select.py
@@ -13,59 +13,83 @@ mydb = mysql.connector.connect(
 dbHaku = mydb.cursor()
 def tulos():
     ugly = dbHaku.fetchall()
-    selection = re.sub(r"[^a-zA-Z0-9 ]", "", str(ugly))
+    selection = re.sub(r"[^a-öA-Ö0-9 ]", "", str(ugly))
     return selection
 
 #HAHMOT:
 def Yrjö():
     dbHaku.execute("SELECT player FROM game WHERE player = 'Yrjö';")
-    print(f"Character: ", tulos())
+    print(f"Character: ",tulos())
     dbHaku.execute("SELECT wallet FROM game WHERE player = 'Yrjö';")
-    print(f"Wallet: ", tulos())
+    print(f"Wallet: ",tulos())
     dbHaku.execute("SELECT airport.name as 'Airport' FROM airport, game WHERE location = ident and player = 'Yrjö';")
-    print(f"Location: ", tulos())
+    print(f"Location: ",tulos())
     return
 def Hasan():
     dbHaku.execute("SELECT player FROM game WHERE player = 'Hasan';")
-    print(f"Character: ", tulos())
+    print(f"Character: ",tulos())
     dbHaku.execute("SELECT wallet FROM game WHERE player = 'Hasan';")
-    print(f"Wallet: ", tulos())
+    print(f"Wallet: ",tulos())
     dbHaku.execute("SELECT airport.name as 'Airport' FROM airport, game WHERE location = ident and player = 'Hasan';")
-    print(f"Location: ", tulos())
+    print(f"Location: ",tulos())
     return
 def SumTinWong():
     dbHaku.execute("SELECT player FROM game WHERE player = 'Sum Tin Wong';")
-    print(f"Character: ", tulos())
+    print(f"Character: ",tulos())
     dbHaku.execute("SELECT wallet FROM game WHERE player = 'Sum Tin Wong';")
-    print(f"Wallet: ", tulos())
+    print(f"Wallet: ",tulos())
     dbHaku.execute("SELECT airport.name as 'Airport' FROM airport, game WHERE location = ident and player = 'Sum Tin Wong';")
-    print(f"Location: ", tulos())
+    print(f"Location: ",tulos())
     return
 
 #INTRO?!?!? Tosi bad veri baad
-print("Welcome to Mars Rush!")
-time.sleep(1)
-print("You have three characters to select with varying difficulties!")
-time.sleep(3)
+game = input("Start the game? [Y/N] ")
+if game == "Y" or game == "y":
+    print("Welcome to Project Mars Demo!")
+    time.sleep(1)
+    print("You have three difficulties, each with a different character!")
+    time.sleep(2)
 
-print("""
-      1. Normal
-      
-      2. Hard
-      
-      3. Extreme!
-""")
+    print("""
+          1. Normal
 
-diff = input("Choose your difficulty: ")
+          2. Hard
 
-if diff == "1" or "Normal" or "normal":
-    Yrjö()
+          3. Extreme!
+    """)
 
-elif diff == "2" or "Hard" or "hard":
-    Hasan()
-
-elif diff == "3" or "Extreme" or "extreme":
-    SumTinWong()
-else:
-    print("Error!")
     diff = input("Choose your difficulty: ")
+
+    while diff != "1" or diff != "2" or diff != "3":
+
+        if diff == "1":
+            Yrjö()
+            confirm = input("Do you want to continue? [Y/N] ")
+            if confirm == "Y" or confirm == "y":
+                break
+            elif confirm == "N" or confirm == "n":
+                diff = input("Choose your difficulty: ")
+        elif diff == "2":
+            Hasan()
+            confirm = input("Do you want to continue? [Y/N] ")
+            if confirm == "Y" or confirm == "y":
+                break
+            elif confirm == "N" or confirm == "n":
+                diff = input("Choose your difficulty: ")
+
+        elif diff == "3":
+            SumTinWong()
+            confirm = input("Do you want to continue? [Y/N] ")
+            if confirm == "Y" or confirm == "y":
+                break
+            elif confirm == "N" or confirm == "n":
+                diff = input("Choose your difficulty: ")
+        elif diff == "Exit" or diff == "exit":
+            exit()
+
+        else:
+            print("Try writing 1, 2 or 3")
+            diff = input("Choose your difficulty: ")
+elif game == "N" or game == "n":
+    print("Bye bye!")
+    exit()
